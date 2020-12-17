23141579604d1540b6a3ec5eb6913e8d30abb098
yuuki-train <yuuki323-sld51@kcf.biglobe.ne.jp>
2020-12-17 17:08:19 +0900

    SpringBootAPI_dbset created

---
+++ src/main/java/com/example/dbset/DbsetApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbsetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dbset/DbsetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbsetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
